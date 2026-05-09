d31c71a3323423d404fddbf552a3fc3049abb126
gabe <gabrieldsuoth@gmail.com>
2026-05-09 09:25:01 +0700

    update

--- Desktop/.config/fish/config.fish
+++ Desktop/.config/fish/config.fish
@@ -1,7 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
 function fish_greeting
    # smth smth
 end
@@ -10,3 +8,7 @@ set -gx MICRO_TRUECOLOR 1
 
 alias venv="source .venv/bin/activate.fish"
 alias fishrc="micro ~/.config/fish/config.fish"
+
+fish_add_path /home/gabe/.spicetify
+
+alias ssh="kitten ssh"
